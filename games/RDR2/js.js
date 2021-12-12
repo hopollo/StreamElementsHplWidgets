92b355f5289d437f8599fc45ad85d188ce30ce7c
HoPollo <hopollotv@gmail.com>
2021-12-12 10:58:56 +0300

    Create js.js

---
+++ games/RDR2/js.js
@@ -0,0 +1,111 @@
+const debug = true;
+const coupleMessages = true;
+let lastMsg;
+let channelName = "";
+
+window.addEventListener('onEventReceived', (obj) => {
+  	const o = obj.detail;
+  	if (debug) console.log(o);
+  
+  	switch(o.listener) {
+      case "message": 
+        	createMessage(o);	
+        break;
+      case "delete-messages":
+        	deleteMessage(o);
+        break;
+    }
+});
+
+window.addEventListener('onWidgetLoad', async (obj) => {
+  channelName = obj.detail.channel.username;
+  // first init
+  await getCurrentGame();
+  
+  // starting timer after
+  setInterval(getCurrentGame, 60 * 1000);
+});
+
+async function getCurrentGame() {
+  try {
+    const fetchedGame = await fetch(`https://decapi.me/twitch/game/${channelName}`).then(res => res.text());
+	if (debug) console.log('recents', fetchedGame);
+    
+    if (fetchedGame === 'Red Dead Redemption 2') {
+      $('.main-container').css('display', 'block');
+      debug && console.log('Game matching, chat enabled');
+    } else {
+      $('.main-container').css('display', 'none');
+      debug && console.log('Wrong game, chat disabled');
+    }
+  } catch (error) { console.error(error); }
+}
+
+function deleteMessage(e) {
+  $(`.${e.event.userId}`).empty();
+}
+
+async function createMessage(e) {
+  if (debug) console.log(e);
+  const evt = e.event.data;
+
+  const banned = ['streamelements', 'wizebot'];
+  const alt = evt.nick;
+
+  if (banned.includes(evt.nick)) return;
+
+  const userId = evt.userId;
+  const color = evt.displayColor;
+  const name = evt.displayName;
+  const msgId = evt.msgId;
+  
+  const text = e.event.renderedText;
+	/*
+  if (name == lastMsg && $('.main-container li:last-child').length !== 0) {
+    const supp = `
+      <li class="message">${text}</li>
+    `;
+
+    $('li:last-child .messageUl').append(supp);
+  } else {
+    const item = `
+      <li class="bubble ${userId}">
+          <div class="name">${name + ': '}</div>
+          <ul class="messageUl">
+              <li class="message">${text}</li>
+          </ul>
+        </div>
+      </li>`;
+    
+    $('.messages').append(item);
+  }
+
+  lastMsg = name;
+  */
+  
+   const item = `
+      <li class="bubble ${userId}">
+          <div class="name">${name + ': '}</div>
+          <div class="message">${text}</div>
+        </div>
+      </li>`;
+    
+  $('.messages').append(item);
+  
+  checkVisibility();
+}
+
+function checkVisibility() {
+  $('.messages').each(function(){
+    $(this).find('li.bubble').each(function(){
+		const self = $(this);
+        if(self.offset().top < 60) {
+          self.addClass('purge');
+
+          setTimeout(() => {
+            self.detach();
+          }, 1000);
+        }
+    });
+  });
+}
